044238eb3448cfe2dbcd844eb35038bf8a99bd2f
jaysvishwagit <jaysvishwa@gmail.com>
2022-03-30 20:06:18 +0530

    home page update

--- resources/views/client/pages/home.blade.php
+++ resources/views/client/pages/home.blade.php
@@ -254,7 +254,7 @@
 								<div class="reviews">
 									<img src="{{$review->feature_image}}" alt="" />
 									<h3>{{$review->title}} <span>{{$review->description}}</span></h3>
-									<p>{!!substr($review->body, 0, 400)!!}</p>
+									<p>{!!substr($review->body, 0, 200)!!}</p>
 								</div><!-- Reviews -->
 							</div>
 							@endforeach
